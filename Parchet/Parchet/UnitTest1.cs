e945127231b2de75747e4d0fb8f8b8b0b62bb783
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-13 06:13:34 +0200

    Pb. Parchet
    Am creat un test care sa verifice calculul suprafetei camerei, apoi
    metoda de calcul a suprafetei camerei. Metodei i-am dat sa returneze 0.
    Normal ca testul nu a trecut.

---
+++ Parchet/Parchet/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parchet
+{
+    [TestClass]
+    public class Parchet
+    {
+        [TestMethod]
+        public void SuprafataCamera()
+        {
+            Parchet obj = new Parchet();
+            double SuprafCamera = obj.CalcSuprCam(5.45, 7.55);
+            Assert.AreEqual(5.45 * 7.55, SuprafCamera);
+        }
+
+        public double CalcSuprCam(double lungime, double latime)
+        {
+            return lungime*latime;
+        }
+    }
+}
