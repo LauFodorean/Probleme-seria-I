f85a577db9be2df972a876ae6a94f0f5ce0a79ba
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-13 05:10:31 +0200

    Pb sportiv
    Am creat primul test. in metoda am dat return 0 si in conclusie testul a
    cazut.

---
+++ Sportiv/Sportiv/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportiv
+{
+    [TestClass]
+    public class Sportiv
+    {
+        [TestMethod]
+        public void CalculTest()
+        {
+            Sportiv sp = new Sportiv();
+            int repetitii = sp.CalculRepetitii(4);
+            Assert.AreEqual(16,repetitii);
+        }
+
+        public int CalculRepetitii(int rep)
+        {
+            return 0;
+        }
+    }
+}
