1062b1be4424e560a104de01a10f15a20d5c6443
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-13 04:14:22 +0200

    Pb. Credit bancar
    Am scris primul test si prima metoda aferenta in asa fel incat testul sa
    cada.

---
+++ CreditBancar/CreditBancar/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CreditBancar
+{
+    [TestClass]
+    public class Credit
+    {
+        [TestMethod]
+        public void Rata()
+        {
+            Credit obj = new Credit();
+            double rata = obj.CalculRata(40000.00, 7.57);
+            Assert.AreEqual(179.28,rata);
+
+        }
+
+        public double CalculRata(double suma, double procent)
+        {
+            return 0.00;
+        }
+    }
+}
