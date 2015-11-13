4526addd6fa58ee4907fa83461ea2e2765799900
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-13 03:38:14 +0200

    Problema pavaj
    Am creat primul test si apoi am scris sub el metoda in asa fel incat
    testul sa cada; valoarea returnata e zero si nu e cea corecta.

---
+++ Pavaj-TDD/Pavaj-TDD/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pavaj_TDD
+{
+    [TestClass]
+    public class Calcule
+    {
+        [TestMethod]
+        public void PavelePe_o_Directie()
+        {
+            Calcule obj = new Calcule();
+            int rezultat = obj.NrDirectie(30, 10);
+            Assert.AreEqual(3, rezultat);
+        }
+
+        public int NrDirectie(int dim1, int dim2)
+        {
+            int numar;
+            if ((dim1 % dim2) > 0)
+                return 0;
+            else
+                return 0;
+        }
+
+
+    }
+}
