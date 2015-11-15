1207028cecc8c108c78dd74f05ca4623df2945fd
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-15 19:49:55 +0200

    Pb. credit bancar
    Alta varianta cu mai multe teste, dar am aceeasi pb cu zecimalele. Te
    rog sa imi dai sugestii !

--- CreditBancar/CreditBancar/UnitTest1.cs
+++ CreditBancar/CreditBancar/UnitTest1.cs
@@ -23,41 +23,76 @@ namespace CreditBancar
         }
 
         [TestMethod]
-        public void RandomRate()
+        public void AssignedMonth()
         {
-            decimal BankRate = RandomBankRate(40000.00m, 20,1,3, 7.57m);
-            Assert.AreEqual(177.67m, BankRate);
-        
+            int Month = TheAssignedMonth(4, 3);
+            Assert.AreEqual(39,Month);
+
         }
 
-        public decimal RandomBankRate(decimal amount, int period, int year, int month, decimal percent)
+        public int TheAssignedMonth(int year, int month)
         {
-            decimal RandomRate=0.00m;
-            if (year == 1)
-                for (int i = 1; i <= month; i++)
-                {
-                    decimal rate = amount / (period * 12) * (1 + (percent / 100));
-                    amount =amount - rate;
-                                        
-                    if (i == month)
-                        RandomRate = rate;
-                    
-                }
-            else
-            {
-                int RequestedMonth = (year*12)+month;
-                for (int i = 1; i <= RequestedMonth; i++)
-                {
-                    decimal rate = amount / (period * 12) * (1 + (percent / 100));
-                    amount = amount - rate;
-                                        
-                    if (i == RequestedMonth)
-                        RandomRate = rate;
-                }
-            }
-            return RandomRate;
-            
+            int TheMonth = (year-1) * 12 + month;
+            return TheMonth;
         }
+
+        [TestMethod]
+        public void RateAssignedMonth()
+        {
+            decimal Rate = TheAssignedMonthRate(40000.00m,20,7.57m,1);
+            Assert.AreEqual(178.4797m, Rate);
+
+        }
+
+        public decimal TheAssignedMonthRate(decimal credit, int period, decimal percent, int month)
+        {
+            decimal ExpectedRate=0.00m;
+            for (int i = 1; i <= month; i++)
+            {
+                decimal rate = credit / (period * 12) * (1 + (percent / 100));
+                credit = credit - rate;
+                if (i == month)
+                    ExpectedRate = rate;
+            }
+            return ExpectedRate;
+        }
+
+        //[TestMethod]
+        //public void RandomRate()
+        //{
+        //    decimal BankRate = RandomBankRate(40000.00m, 20.00m,1.00m,3.00m, 7.57m);
+        //    Assert.AreEqual(177.67m, BankRate);
+        
+        //}
+                      
+        //public decimal RandomBankRate(decimal amount, decimal period, decimal year, decimal month, decimal percent)
+        //{
+        //    decimal RandomRate=0.00m;
+        //    if (year == 1)
+        //        for (int i = 1; i <= month; i++)
+        //        {
+        //            decimal rate = amount / (period * 12) * (1 + (percent / 100));
+        //            amount =amount - rate;
+                                        
+        //            if (i == month)
+        //                RandomRate = rate;
+                    
+        //        }
+        //    else
+        //    {
+        //        decimal RequestedMonth = (year*12)+month;
+        //        for (int i = 1; i <= RequestedMonth; i++)
+        //        {
+        //            decimal rate = amount / (period * 12) * (1 + (percent / 100));
+        //            amount = amount - rate;
+                                        
+        //            if (i == RequestedMonth)
+        //                RandomRate = rate;
+        //        }
+        //    }
+        //    return RandomRate;
+            
+        //}
 
 
     }
