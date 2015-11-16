ad0b6c940baee9c1162ebe882654fef1fcec4191
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-16 22:39:47 +0200

    Teren Fermier
    Am scris prima metode de testare si metoda clasei. Testul nu trece.

---
+++ TerenFermier/TerenFermier/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmerField
+{
+    [TestClass]
+    public class Farmer
+    {
+        [TestMethod]
+        public void Side()
+        {
+            double OriginalSide = CalculusOfSide(2, 1);
+            Assert.AreEqual(1, OriginalSide);
+        }
+
+        public int CalculusOfSide(int TotalArea, int KnownLenght)
+        {
+            return 0;
+        }
+    }
+}
+
