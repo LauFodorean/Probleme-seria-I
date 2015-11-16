7b084a5c4af6ab64e11f4c20e50c5e277c92526c
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-16 23:36:17 +0200

    FizzBUzz
    Problema rezolvata

--- FizzBuzz/FizzBuzz/UnitTest1.cs
+++ FizzBuzz/FizzBuzz/UnitTest1.cs
@@ -9,13 +9,39 @@ namespace FizzBuzz
         [TestMethod]
         public void TestFizz()
         {
-            String result = FizzBuzzMethod(15, 3, 5);
+            String result = FizzBuzzMethod(9, 3, 5);
             Assert.AreEqual("Fizz", result);
+        }
+
+        [TestMethod]
+        public void TestBuzz()
+        {
+            String result = FizzBuzzMethod(10, 3, 5);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            String result = FizzBuzzMethod(15, 3, 5);
+            Assert.AreEqual("FizzBuzz", result);
         }
 
         public string FizzBuzzMethod(int GivenNumber, int FirstDivisor, int SecondDivisor)
         {
-            return "Buzz";
+            String ActualResult=" ";
+            double FirstReminder = GivenNumber % FirstDivisor;
+            double SecondReminder = GivenNumber % SecondDivisor;
+            if (FirstReminder == 0 && SecondReminder == 0)
+                ActualResult = "FizzBuzz";
+            else
+                if (FirstReminder == 0 && SecondReminder != 0)
+                    ActualResult = "Fizz";
+                else
+                    if (FirstReminder != 0 && SecondReminder == 0)
+                    ActualResult = "Buzz";
+
+            return ActualResult;
         }
     }
 }
