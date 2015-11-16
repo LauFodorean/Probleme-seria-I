839032633d656524dcc3d50bf1d1fca9467607a8
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-16 23:15:32 +0200

    FizzBuzz
    Prima metoda de testare .. nu a trecut deoarece astepta ca si rezultat
    Fizz, iar mie metoda scrisa mai jos mi-a returnat buzz.

---
+++ FizzBuzz/FizzBuzz/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class NumberToBeTested
+    {
+        [TestMethod]
+        public void TestFizz()
+        {
+            String result = FizzBuzzMethod(15, 3, 5);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        public string FizzBuzzMethod(int GivenNumber, int FirstDivisor, int SecondDivisor)
+        {
+            return "Buzz";
+        }
+    }
+}
