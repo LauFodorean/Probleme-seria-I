339078230cfdac9e72f648e274299faa37c071fa
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-22 17:42:51 +0200

    Chirie
    Am scris primul test. El nu a trecut deoarece metoda de calcul a chiriei
    returneaza zero.

---
+++ Rent/Rent/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rent
+{
+    [TestClass]
+    public class RentTest1
+    {
+        [TestMethod]
+        public void RentDelayOneToTenDays()
+        {
+            decimal rent = RentCalculation(100, 1);
+            Assert.AreEqual(102, rent);
+        }
+
+        public decimal RentCalculation(decimal usualRent, int delayInPayments)
+        {
+            return 0;
+        }
+    }
+}
