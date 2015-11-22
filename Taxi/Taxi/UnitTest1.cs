69f75c27f3d813a1a183cfb083f480452b98c33f
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-22 20:04:51 +0200

    Taxi
    Am scris primul test si metoda de calcul. Metoda de calcul returneaza
    zero iar testul nu trece.

---
+++ Taxi/Taxi/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taxi
+{
+    [TestClass]
+    public class TaxiTest
+    {
+        [TestMethod]
+        public void DayFeeCalculation()
+        {
+            double fee = TaxiFeeCalculation(10, 9);
+            Assert.AreEqual(50, fee);
+        }
+
+        public double TaxiFeeCalculation(double distance, int hour)
+        {
+            return 0;
+        }
+    }
+}
