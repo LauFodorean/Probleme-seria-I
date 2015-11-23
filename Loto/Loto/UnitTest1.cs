ac656ea8ddc5c532332d163b70201c299b7d3d23
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-23 07:37:44 +0200

    Loto
    Am scris un test pentru varianta 2 din 6. Metodei i-am dat sa returneze
    zero. Testu nu a trecut .. e ok .

---
+++ Loto/Loto/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loto
+{
+    [TestClass]
+    public class LotoUnitTest
+    {
+        [TestMethod]
+        public void TwoOfSixTestMethod()
+        {
+            int numberOfVariants = VariantsCalculation( 6 , 2);
+            Assert.AreEqual( 15 , numberOfVariants );
+        }
+
+        public int VariantsCalculation(int totalNumbers, int combinationNumber)
+        {
+            return 0;
+        }
+    }
+}
