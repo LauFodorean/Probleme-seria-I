b1205dc5faf77896d1c06435cd66fe3369fe34d6
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-23 05:41:34 +0200

    Squares
    Problema rezolvata.

---
+++ NumberOoSquares/NumberOoSquares/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumberOfSquares
+{
+    [TestClass]
+    public class SquaresUnitTest
+    {
+        [TestMethod]
+        public void FirstTestMethod()
+        {
+            int number = SquareNumberCalculation(8);
+            Assert.AreEqual(204, number);
+        }
+            
+        public int SquareNumberCalculation(int dimension)
+        {
+            int numberOfSquares = 0;
+            for (int i = 1; i<9; i++)
+            {
+                numberOfSquares = numberOfSquares + i * i ;
+
+            }
+            return numberOfSquares;
+        }
+    }
+}
