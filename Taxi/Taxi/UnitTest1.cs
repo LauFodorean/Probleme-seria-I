c34950899a552dd598b9685541f167728bdb8373
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-24 07:01:22 +0200

    Taxi
    Am introdus o metoda pentru calculul distantei dus-intors si am ellimnat
    introducerea tarifelor in metoda de calcul a sumei de plata, introducand
    parametri suplimentari pentru functie si introducand in teste tarifele
    respective, la fel ca si distanta si ora cursei.

--- Taxi/Taxi/UnitTest1.cs
+++ Taxi/Taxi/UnitTest1.cs
@@ -9,55 +9,60 @@ namespace Taxi
         [TestMethod]
         public void DayFeeCalculation()
         {
-            double fee = TaxiFeeCalculation(10, 9);
+            double fee = TaxiFeeCalculation(10, 9, 5, 4, 3, 7, 5, 4);
             Assert.AreEqual(50, fee);
         }
 
         [TestMethod]
         public void DayFeeCalculationLongDistance()
         {
-            double fee = TaxiFeeCalculation(100, 9);
+            double fee = TaxiFeeCalculation(100, 9, 5, 4, 3, 7, 5, 4);
             Assert.AreEqual(600, fee);
         }
 
         [TestMethod]
         public void NightFeeCalculationShortDistance()
         {
-            double fee = TaxiFeeCalculation(10, 7);
+            double fee = TaxiFeeCalculation(10, 7, 5, 4, 3, 7, 5, 4);
             Assert.AreEqual(70, fee);
         }
 
         [TestMethod]
         public void NightFeeCalculationLongDistance()
         {
-            double fee = TaxiFeeCalculation(100, 7);
+            double fee = TaxiFeeCalculation(100, 7, 5, 4, 3, 7, 5, 4);
             Assert.AreEqual(800, fee);
         }
 
-        public double TaxiFeeCalculation(double distance, int hour)
+        public double TaxiFeeCalculation(double distance, int hour, int dayShortTripFee, int dayMediumTripFee, int dayLongTripFee, int nightShortTripFee, int nightMediumTripFee, int nightLongTripFee)
         {
             double fee = 0;
             if (hour >= 8 & hour < 21)
             {
                 if (distance <= 21)
-                    fee = distance * 5;
+                    fee = distance * dayShortTripFee;
                 else
                     if (distance <= 60)
-                        fee = distance * 4 * 2;
+                        fee = TwoWay(distance) * dayMediumTripFee;
                     else
-                        fee = distance * 3 * 2;
+                        fee = TwoWay(distance) * dayLongTripFee;
             }
             else
             {
                 if (distance <= 21)
-                    fee = distance * 7;
+                    fee = distance * nightShortTripFee;
                 else
                     if (distance <= 60)
-                        fee = distance * 5 * 2;
+                        fee = TwoWay(distance) * nightMediumTripFee;
                     else
-                        fee = distance * 4 * 2;
+                        fee = TwoWay(distance) * nightLongTripFee;
             }
             return fee;
+        }
+
+        private static double TwoWay(double distance)
+        {
+            return distance * 2;
         }
     }
 }
