5414f30f6edd312298fa0373e10d101dd0b2c1ae
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-26 06:39:21 +0200

    Anagrame
    Am scris un test si o metoda de calcul a numaruluide anagramari posibile
    pentru un cuvant format din doua litere.

---
+++ Anagramari/Anagramari/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagramari
+{
+    [TestClass]
+    public class AnagramUnitTest
+    {
+        [TestMethod]
+        public void AnagramTestMethod()
+        {
+            Assert.AreEqual(2,AnagramCalculation("da"));
+        }
+
+        int AnagramCalculation(string givenWord)
+        {
+            int wordLenght = 0;
+            wordLenght = givenWord.Length;
+            return (wordLenght * wordLenght)-2;
+        }
+    }
+}
