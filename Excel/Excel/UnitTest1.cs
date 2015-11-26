7e9c581173d952b34cd63d00d2170c3424d23e0c
LauFodorean <laurean_fodorean@yahoo.com>
2015-11-26 14:01:33 +0200

    Excel
    Am scris testul pentru coloane care au numarul multiplu de 26.

---
+++ Excel/Excel/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excel
+{
+    [TestClass]
+    public class ExcelUnitTest
+    {
+        [TestMethod]
+        public void NumberUnderTwentysixTestMethod()
+        {
+            char number = ColumnNumber(6);
+            Assert.AreEqual('F',number);
+        }
+
+        [TestMethod]
+        public void NumberMultipleOfTwentysixTestMethod()
+        {
+            char number = ColumnNumber(27);
+            Assert.AreEqual('A','A', number);
+        }
+
+        public char ColumnNumber(int number)
+        {
+            char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] columnNumber = new char[1000];
+            int cateDivision = 0;
+            int restOfDivision = 0;
+            if (number <= 26)
+                columnNumber[number - 1] = alphabet[number - 1];
+            else
+            {
+                cateDivision = number / 26;
+                restOfDivision = number % 26;
+                for (int i = 0; i < cateDivision; i++)
+                    columnNumber[i] = alphabet[i];
+            }
+            return columnNumber[number - 1];
+
+        }
+    }
+}
