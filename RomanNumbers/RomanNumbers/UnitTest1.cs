bd08e2e3c89f195fe7553d5fc9d88c90d5c5219e
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-01 17:56:18 +0200

    Roman numbers
    Test pentru numerele de la 1 la 10

---
+++ RomanNumbers/RomanNumbers/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumbers
+{
+    [TestClass]
+    public class RomanNumbersTest
+    {
+        [TestMethod]
+        public void FirstTenNumbersTestMethod()
+        {
+            string number = CalculateRomanNumbers(1);
+            Assert.AreEqual("I", number);
+        }
+
+        public string CalculateRomanNumbers(int number)
+        {
+            string[] romanNumber = new string[20];
+            string[] digitsAndNumbers =  {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "L", "C"};
+            switch (number)
+            {
+                case 1:
+                    romanNumber[0] = digitsAndNumbers[0];
+                    break;
+            }
+
+            
+            return romanNumber[0];
+        }
+    }
+}
