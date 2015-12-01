6eb7885ba62ce142c51a15ad4d0048c011133056
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-01 18:20:05 +0200

    Roman Numbers
    La metoda 1 am modificat codul. Numarul roman l-am declarat ca si o
    variabila de tip string. Am lasat ca si array lista cu numerele romane
    incluzand aici 50 si 100.

--- RomanNumbers/RomanNumbers/UnitTest1.cs
+++ RomanNumbers/RomanNumbers/UnitTest1.cs
@@ -13,19 +13,34 @@ namespace RomanNumbers
             Assert.AreEqual("I", number);
         }
 
+        [TestMethod]
+        public void NumbersElevenToTwentyTestMethod()
+        {
+            string number = CalculateRomanNumbers(11);
+            Assert.AreEqual("XI", number);
+        }
+
         public string CalculateRomanNumbers(int number)
         {
-            string[] romanNumber = new string[20];
+            string romanNumber = "";
             string[] digitsAndNumbers =  {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "L", "C"};
             switch (number)
             {
                 case 1:
-                    romanNumber[0] = digitsAndNumbers[0];
+                    romanNumber = romanNumber + digitsAndNumbers[0];
                     break;
+                
+                //case 2:
+                //    int divisionByTen = number / 10;
+                //    int remainder = number % 10;
+                //    romanNumber[0] = digitsAndNumbers[9];
+                //    romanNumber[1] = digitsAndNumbers[remainder];
+                    //break;
+
             }
 
             
-            return romanNumber[0];
+            return romanNumber;
         }
     }
 }
