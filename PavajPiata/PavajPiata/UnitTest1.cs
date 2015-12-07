f511dd379c3a8583fef2d7d652b4ebfda5a97172
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-07 05:27:48 +0200

    Pavaj - problema refacuta
    Primul test trecut. Am calculat numarul de pavele pe lungimea pietei. Am
    folosit clasa Math si functia Ceiling.

---
+++ PavajPiata/PavajPiata/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PavajPiata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NumberOfPavemetsOnSquareLenghtTestMethod()
+        {
+            Assert.AreEqual(3, OneDirectionPavemetCalculation(25, 15, 10));
+        }
+
+        public int OneDirectionPavemetCalculation(double lenghtSquareDimension, double WidthSquareDimension, int pavementDimension)
+        {
+            double numberOfPavements = lenghtSquareDimension / pavementDimension;
+            numberOfPavements = Math.Ceiling(numberOfPavements);
+            return (int)numberOfPavements; 
+        }
+    }
+}
