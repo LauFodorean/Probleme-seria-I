bbe21378e728e6a91a3af8fe1138463c8101439a
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-07 06:11:29 +0200

    Parquet Calculation
    Test scris pentru varianta in care avem o camera cu latimea unei bucati
    de parchet .

--- Parchet/Parchet/UnitTest1.cs
+++ Parchet/Parchet/UnitTest1.cs
@@ -7,53 +7,56 @@ namespace Parchet
     public class Parchet
     {
         [TestMethod]
-        public void SuprafataCamera()
+        public void PiecesOfParquetOnOneDirection()
         {
+
+            Assert.AreEqual(8, ParquetCalculation(7.55, 0.15, 1.00, 0.15));
+        }
+
+        public double ParquetCalculation(double roomLenght, double roomWidth, double parquetLenght, double parquetWidth)
+        {
+            double roomArea = roomLenght * roomWidth;
+            double pieceOfParquetArea = parquetLenght * parquetWidth;
+            double numberOfParquetPieces = roomArea / pieceOfParquetArea;
+            numberOfParquetPieces = Math.Ceiling(numberOfParquetPieces);
+            return (int)numberOfParquetPieces;
+        }
+
+        //[TestMethod]
+        //public void DimParchet()
+        //{
             
-            double SuprafCamera = CalcSuprCam(5.45, 7.55);
-            Assert.AreEqual(5.45 * 7.55, SuprafCamera);
-        }
+        //    double Supraf = CalcSuprP(1.00, 0.25);
+        //    Assert.AreEqual(1.00 * 0.25, Supraf);
+        //}
 
-        public double CalcSuprCam(double lungime, double latime)
-        {
-            return lungime*latime;
-        }
+        //public double CalcSuprP(double lungime, double latime)
+        //{
+        //    return lungime*latime;
+        //}
 
-        [TestMethod]
-        public void DimParchet()
-        {
+        //[TestMethod]
+        //public void NumarBucati()
+        //{
             
-            double Supraf = CalcSuprP(1.00, 0.25);
-            Assert.AreEqual(1.00 * 0.25, Supraf);
-        }
+        //    double BucatiParchet = BucParchet(5.45, 7.55, 1.00, 0.25);
+        //    Assert.AreEqual(190, BucatiParchet);
+        //}
 
-        public double CalcSuprP(double lungime, double latime)
-        {
-            return lungime*latime;
-        }
-
-        [TestMethod]
-        public void NumarBucati()
-        {
-            
-            double BucatiParchet = BucParchet(5.45, 7.55, 1.00, 0.25);
-            Assert.AreEqual(190, BucatiParchet);
-        }
-
-        public int BucParchet(double lungime, double latime, double lungimeP, double latimeP) //lungimeP si latimeP sunt dimensiunile bucatii de parchet
-        {
-            double SuprafataNec = lungime * latime * 1.15;
-            double dimensiune = lungimeP * latimeP;
-            double nr1 = SuprafataNec / dimensiune;
-            int bucatiparchet;
-            int nr2 = Convert.ToInt32(nr1);
-            double diferenta = nr1%nr2;
-            if (diferenta>0)
-                bucatiparchet=nr2+1;
-            else
-                bucatiparchet=nr2;
-            return bucatiparchet;
-        }
+        //public int BucParchet(double lungime, double latime, double lungimeP, double latimeP) //lungimeP si latimeP sunt dimensiunile bucatii de parchet
+        //{
+        //    double SuprafataNec = lungime * latime * 1.15;
+        //    double dimensiune = lungimeP * latimeP;
+        //    double nr1 = SuprafataNec / dimensiune;
+        //    int bucatiparchet;
+        //    int nr2 = Convert.ToInt32(nr1);
+        //    double diferenta = nr1%nr2;
+        //    if (diferenta>0)
+        //        bucatiparchet=nr2+1;
+        //    else
+        //        bucatiparchet=nr2;
+        //    return bucatiparchet;
+        //}
 
      }
 }
