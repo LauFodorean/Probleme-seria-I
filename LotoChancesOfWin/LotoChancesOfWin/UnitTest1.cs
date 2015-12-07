36f2f5f1662ef6edc98b13035c4cda33851a599e
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-07 07:44:03 +0200

    Loto Canses Of Win
    Am scris un test pentru o varianta mai simplu de calculat.
    Am creat o metoda pentru calculul factorialului.

---
+++ LotoChancesOfWin/LotoChancesOfWin/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LotoChancesOfWin
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TwoOfSixChansesTest()
+        {
+            Assert.AreEqual(15, ChansesCalculation(2, 6));
+        }
+
+        public int ChansesCalculation(int numbersInCombination, int numbersToChoseFrom)
+        {
+            int chansesOfWin = Factorial(numbersToChoseFrom) / (Factorial(numbersInCombination) * Factorial(numbersToChoseFrom - numbersInCombination));
+            return chansesOfWin;
+        }
+
+        private int Factorial(int number)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+                factorial = factorial * i;
+            return factorial;
+        }
+    }
+}
