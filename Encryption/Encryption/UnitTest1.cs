411a7c4a678a0ffbaceef2bfec80d333b4a90d85
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-13 23:34:10 +0200

    Encryption
    Am scris un test care verifica eliminarea spatiilor si a punctuatiei din
    fraza.

---
+++ Encryption/Encryption/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Encryption
+{
+    [TestClass]
+    public class EncryptUnitTest
+    {
+
+        [TestMethod]
+        public void PunctuationAndBlankRemovalTestMethod()
+        {
+
+            string message = EncryptMessage(" 123 45.67");
+            Assert.AreEqual("1234567", message);
+
+        }
+
+        public string EncryptMessage(string pmessage)
+        {
+            string encryptedText;
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            encryptedText = pmessage.Replace(".", "").Replace(" ", "").Replace("-", "").Replace(",", "").Replace(";", "");
+            return encryptedText;
+        }
+
+    }
+}
