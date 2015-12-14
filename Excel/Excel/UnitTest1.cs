1dc9dd177c4674a3868abf328e5388a48dd94e23
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-14 08:00:01 +0200

    Excel
    Test pentru numere peste 26. Am luat numarul 78 pentru care am scris
    testul. Am scris si codul aferent. Copiez din string in matrice. Nu
    merge deocamdata.

--- Excel/Excel/UnitTest1.cs
+++ Excel/Excel/UnitTest1.cs
@@ -12,6 +12,11 @@ namespace Excel
             Assert.AreEqual("F",ColumnNumber(5));
         }
 
+        [TestMethod]
+        public void NumberSeventyEightTestMethod()
+        {
+            Assert.AreEqual("BZ", ColumnNumber(78));
+        }
         
 
        
@@ -19,7 +24,7 @@ namespace Excel
         public string ColumnNumber(int number)
         {
            string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-           string[,] matrix1;
+           string[,] matrix1 = new string [26 , 26];
            string[,] matrix2;
 
            string wantedColumndNumber = "";
@@ -27,8 +32,21 @@ namespace Excel
            int restOfDivision = 0;
            if (number < 26)
                wantedColumndNumber = wantedColumndNumber + alphabet[number];
-           
-           return wantedColumndNumber;
+           else
+           {
+               cateDivision = number / 26;
+               restOfDivision = number % 26;
+               int numberOfRows = cateDivision;
+               if (restOfDivision != 0)
+                   numberOfRows = cateDivision + 1;
+               for (int i = 0; i < numberOfRows; i++)
+                   for (int j = 0; j < 26; j++)
+                       matrix1[i, j] = alphabet[i] + alphabet[j];
+               wantedColumndNumber = matrix1[cateDivision, restOfDivision];
+               }
+
+
+            return wantedColumndNumber;
 
         }
     }
