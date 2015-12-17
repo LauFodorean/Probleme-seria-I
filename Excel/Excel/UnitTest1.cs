660345d36ea12369cd5f552849b419aac0928825
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-17 08:20:37 +0200

    Excel
    Am incercat sa simplific codul dar nu imi mai merg doua dintre teste.
    Rationamentul imi pare coresct.

--- Excel/Excel/UnitTest1.cs
+++ Excel/Excel/UnitTest1.cs
@@ -24,19 +24,36 @@ namespace Excel
             Assert.AreEqual("AZ", ColumnNumber(52));
         }
 
+
         public string ColumnNumber(int number)
         {
            string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
            string wantedColumndNumber = "";
-           int cateDivision = number / 26;
-           int restOfDivision = number % 26;
-           if (number <= 26)
-               wantedColumndNumber = alphabet[number - 1];
-           else
-               if (restOfDivision != 0)
-                   wantedColumndNumber = alphabet[cateDivision - 1] + alphabet[restOfDivision - 1];
-               else
-                   wantedColumndNumber = alphabet[cateDivision - 2] + alphabet[25];
+
+
+           while (number > 26)
+           {
+               int cateDivision = 0;
+               int restOfDivision = 0;
+               cateDivision = number / 26;
+               if (cateDivision > 26)
+               {
+                   restOfDivision = number % 26;
+                   number = cateDivision;
+
+               }
+               wantedColumndNumber = alphabet[restOfDivision - 1] + wantedColumndNumber;
+           }
+          
+
+           wantedColumndNumber = alphabet[number - 1] + wantedColumndNumber;
+           //if (number <= 26)
+           //    wantedColumndNumber = alphabet[number - 1];
+           //else
+           //    if (restOfDivision != 0)
+           //        wantedColumndNumber = alphabet[cateDivision - 1] + alphabet[restOfDivision - 1];
+           //    else
+           //        wantedColumndNumber = alphabet[cateDivision - 2] + alphabet[25];
            return wantedColumndNumber;
      
 
