bb6fefa71ec2791c59606b256bae4e9f8b1f1276
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-17 07:20:31 +0200

    Excel
    Am scris un test pentru numar de coloana peste 26. Am scris codul
    aferent pentru a trece testul.

--- Excel/Excel/UnitTest1.cs
+++ Excel/Excel/UnitTest1.cs
@@ -12,14 +12,25 @@ namespace Excel
             Assert.AreEqual("E",ColumnNumber(5));
         }
 
+        [TestMethod]
+        public void NumberThirtyTwoTestMethod()
+        {
+            Assert.AreEqual("AF", ColumnNumber(32));
+        }
+
+
         public string ColumnNumber(int number)
         {
            string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
            string wantedColumndNumber = "";
-            
-           wantedColumndNumber = alphabet[number - 1];
-           
+           int cateDivision = number / 26;
+           int restOfDivision = number % 26;
+           if (number <= 26)
+               wantedColumndNumber = alphabet[number - 1];
+           else
+               wantedColumndNumber = alphabet[cateDivision - 1] + alphabet[restOfDivision - 1];
            return wantedColumndNumber;
+     
 
         }
     }
