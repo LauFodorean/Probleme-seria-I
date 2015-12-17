4e35db430ce94ee1408b945c7748accb1d0bd737
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-17 07:18:02 +0200

    Excel
    Am sters ceea ce era facut inainte cu matrice. Am luat un prin caz de
    test. numarul de coloana e sub 26.

--- Excel/Excel/UnitTest1.cs
+++ Excel/Excel/UnitTest1.cs
@@ -9,44 +9,17 @@ namespace Excel
         [TestMethod]
         public void NumberUnderTwentysixTestMethod()
         {
-            Assert.AreEqual("F",ColumnNumber(5));
+            Assert.AreEqual("E",ColumnNumber(5));
         }
-
-        [TestMethod]
-        public void NumberSeventyEightTestMethod()
-        {
-            Assert.AreEqual("BZ", ColumnNumber(78));
-        }
-        
-
-       
 
         public string ColumnNumber(int number)
         {
            string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-           string[,] matrix1 = new string [26 , 26];
-           string[,] matrix2;
-
            string wantedColumndNumber = "";
-           int cateDivision = 0;
-           int restOfDivision = 0;
-           if (number < 26)
-               wantedColumndNumber = wantedColumndNumber + alphabet[number];
-           else
-           {
-               cateDivision = number / 26;
-               restOfDivision = number % 26;
-               int numberOfRows = cateDivision;
-               if (restOfDivision != 0)
-                   numberOfRows = cateDivision + 1;
-               for (int i = 0; i < numberOfRows; i++)
-                   for (int j = 0; j < 26; j++)
-                       matrix1[i, j] = alphabet[i] + alphabet[j];
-               wantedColumndNumber = matrix1[cateDivision, restOfDivision];
-               }
-
-
-            return wantedColumndNumber;
+            
+           wantedColumndNumber = alphabet[number - 1];
+           
+           return wantedColumndNumber;
 
         }
     }
