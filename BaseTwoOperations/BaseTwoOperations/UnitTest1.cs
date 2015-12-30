e11466da97281ba590937b12843bb9a25a367e28
LauFodorean <laurean_fodorean@yahoo.com>
2015-12-30 12:31:29 +0200

    Operatii baza 2
    Am scris primul test si o prima metoda de transformare a unui numar dat
    in baza 2.

---
+++ BaseTwoOperations/BaseTwoOperations/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseTwoOperations
+{
+    [TestClass]
+    public class BaseTwoOperationsUnitTest
+    {
+        [TestMethod]
+        public void NumberConversionInBinary()
+        {
+            Assert.AreEqual(0, Conversion(14));
+        }
+
+
+        public byte Conversion(int number)
+        {
+            
+            byte[] convertedNumber = new byte[8] {0,0,0,0,0,0,0,0};
+            int restOfDivision, cateOfDivision;
+            for (int i = 7; i >= 0; i--)
+            {
+                restOfDivision = number % 2;
+                cateOfDivision = number / 2;
+
+                if (restOfDivision != 0)
+                    convertedNumber[i] = 1;
+
+                number = cateOfDivision;
+            }
+                
+                return convertedNumber[3];
+        }
+    }
+}
