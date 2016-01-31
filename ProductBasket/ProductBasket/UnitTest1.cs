c94a0c0a5d328291644f91f451f41944c329e5be
LauFodorean <laurean_fodorean@yahoo.com>
2016-02-01 00:52:53 +0200

    ProductBasket
    Pretul cel mai mic.

--- ProductBasket/ProductBasket/UnitTest1.cs
+++ ProductBasket/ProductBasket/UnitTest1.cs
@@ -12,14 +12,18 @@ namespace ProductBasket
             Assert.AreEqual(300, CalculateBasketTotalPrice(new double[] {150, 100, 50}));
         }
 
+        [TestMethod]
+        public void CheapestProductInBasket()
+        {
+            Assert.AreEqual(1, IndicateCheapestProductInBasket(new double[] { 150, 1, 100, 50 }));
+        }
+
         //struct product
         //{
         //    public string name;
         //    public double price;
         //}
-
         
-
         public double CalculateBasketTotalPrice(double[] p)
         {
             double totalPrice = 0;
@@ -27,5 +31,15 @@ namespace ProductBasket
                 totalPrice = totalPrice + (double)p[i];
             return totalPrice;
         }
+
+        public double IndicateCheapestProductInBasket(double[] p)
+        {
+            double lowestPrice = p[0];
+            for (int i = 1; i < p.Length; i++)
+                if (p[i] < lowestPrice)
+                    lowestPrice = p[i];
+            return lowestPrice;
+        }
+
     }
 }
