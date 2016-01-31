2921f18ecc870a4af50432eeea0170d458600876
LauFodorean <laurean_fodorean@yahoo.com>
2016-02-01 00:44:53 +0200

    ProductBasket
    Suma produselor din cos.

---
+++ ProductBasket/ProductBasket/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductBasket
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TotalPriceOfProductsInBasket()
+        {
+            Assert.AreEqual(300, CalculateBasketTotalPrice(new double[] {150, 100, 50}));
+        }
+
+        //struct product
+        //{
+        //    public string name;
+        //    public double price;
+        //}
+
+        
+
+        public double CalculateBasketTotalPrice(double[] p)
+        {
+            double totalPrice = 0;
+            for (int i = 0; i < p.Length; i++)
+                totalPrice = totalPrice + (double)p[i];
+            return totalPrice;
+        }
+    }
+}
