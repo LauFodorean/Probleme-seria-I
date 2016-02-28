8c89e47e99b038defe2e8fd530acd9c907cd5481
LauFodorean <laurean_fodorean@yahoo.com>
2016-02-28 18:09:22 +0200

    Password
    Metoda care verifica daca aprola e formata doar din litere mici .

--- Password/Password/UnitTest1.cs
+++ Password/Password/UnitTest1.cs
@@ -34,12 +34,38 @@ namespace Password
             Assert.AreEqual(12, GeneratePassword(12).Length);
         }
 
+        [TestMethod]
+        public void CheckIfPasswordHasOnlySmallLetters()
+        {
+            Assert.AreEqual(12, GeneratePassword(10));
+        }
+
+        [TestMethod]
+        public void CheckForSmallLetters()
+        {
+            Assert.AreEqual(true, CheckPasswordToSeeIfITHasOnlySmallLetters("abc"));
+        }
+
+        public bool CheckPasswordToSeeIfITHasOnlySmallLetters(string password)
+        {
+            bool smallLetter = true;
+            for (int i = 0; i < password.Length; i++)
+                if (char.IsUpper(password[i]))
+                    smallLetter = false;
+            return smallLetter;
+        }
+
         public string GeneratePassword(int numberOfCharacters)
         {
             string password = "";
+            var character = new Random();
+            char passwordCharacter;
             for (int i = 0; i < numberOfCharacters; i++)
-                password = password + 'a';
-
+            {
+                passwordCharacter = (char)character.Next('a', 'z');
+                password = password + passwordCharacter;
+                
+            }
             return password;
         }
         
