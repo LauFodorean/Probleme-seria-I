ca19112bed275fb58dc7be0368177c6975b7043f
LauFodorean <laurean_fodorean@yahoo.com>
2016-03-07 08:17:49 +0200

    Password
    Generare de symboluri

--- Password/Password/UnitTest1.cs
+++ Password/Password/UnitTest1.cs
@@ -92,6 +92,24 @@ namespace Password
         }
 
         [TestMethod]
+        public void CheckPaswordForNumberofFigures()
+        {
+            Assert.AreEqual(3, GetNumberOfFigures("1abD2c3F"));
+        }
+
+        [TestMethod]
+        public void CheckForNumberofSymbols()
+        {
+            Assert.AreEqual(3, GetNumberOfSymbols("1ab#D2c3@F!"));
+        }
+
+        [TestMethod]
+        public void CheckSymbolsGeneration()
+        {
+            Assert.AreEqual(6, GetNumberOfSymbols(GenerateSymbols(6)));
+        }
+
+        [TestMethod]
         public void CheckIfTwoGeneratedPasswordsAreRandom()
         {
             string firstPassword ="";
@@ -114,6 +132,26 @@ namespace Password
             for (int i = 0; i < password.Length; i++)
                 if (char.IsUpper(password[i]))
                     number++;
+            return number;
+        }
+
+        public int GetNumberOfFigures(string password)
+        {
+            int number = 0;
+            for (int i = 0; i < password.Length; i++)
+                if (char.IsDigit(password[i]))
+                    number++;
+            return number;
+        }
+
+        public int GetNumberOfSymbols(string password)
+        {
+            int number = 0;
+            string symbols = "!@#$%^&*_+-?=:";
+            for (int i = 0; i < password.Length; i++)
+                for (int j = 0; j < symbols.Length; j++ )
+                    if (password[i]==symbols[j])
+                        number++;
             return number;
         }
 
@@ -142,22 +180,55 @@ namespace Password
 
         public string GenerateSmallLetters(int numberOfCharacters)
         {
-            string smallLettersPassword = "";
+            string smallLetters = "";
             var character = new Random();
             char passwordCharacter;
             for (int i = 0; i < numberOfCharacters; i++)
             {
                 passwordCharacter = (char)character.Next('a', 'z');
-                smallLettersPassword = smallLettersPassword + passwordCharacter;
+                smallLetters = smallLetters + passwordCharacter;
 
             }
-            return smallLettersPassword;
+            return smallLetters;
+        }
+
+        public string GenerateFigures(int numberOfCharacters)
+        {
+            string figures = "";
+            var character = new Random();
+            char passwordCharacter;
+            for (int i = 0; i < numberOfCharacters; i++)
+            {
+                passwordCharacter = (char)character.Next('0', '9');
+                figures = figures + passwordCharacter;
+
+            }
+            return figures;
+        }
+
+        public string GenerateSymbols(int numberOfCharacters)
+        {
+            string passwordSymbols = "";
+            //string symbols = "!@#$%^&*-+_?=:";
+            var character = new Random();
+            char passwordCharacter;
+            for (int i = 0; i < numberOfCharacters; i++)
+            {
+                passwordCharacter = (char)character.Next('!','&');
+                //for (int j = 0; j < symbols.Length; j++)
+                //    if (passwordCharacter == symbols[j])
+                        passwordSymbols = passwordSymbols + passwordCharacter;
+                    //else
+                    //    i--;
+
+            }
+            return passwordSymbols;
         }
 
         public string GeneratePassword(passwordsettings numberOfCharacters)
         {
             string password = "";
-            password = GenerateSmallLetters(numberOfCharacters.numberOfSmallLetters) + GenerateBigLetters(numberOfCharacters.numberOfBigLetters);
+            password = GenerateSmallLetters(numberOfCharacters.numberOfSmallLetters) + GenerateBigLetters(numberOfCharacters.numberOfBigLetters) + GenerateFigures(numberOfCharacters.numberOfFigures);
             return password;
         }
         
