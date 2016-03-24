c231e90608d84fe9a4ccf7ce5982bd67f64318d5
LauFodorean <laurean_fodorean@yahoo.com>
2016-03-24 08:14:03 +0200

    Reversed String
    NU merge .  Trebuie lucrat la ea inca.

---
+++ ReversedString/ReversedString/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReversedString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckReversedSring()
+        {
+            Assert.AreEqual("gfedcba", ReverseString("abcdefg"));
+        }
+
+        public string ReverseString(string givenString) 
+        {
+            int n = givenString.Length;
+            return ReversedString (givenString, ref n);
+        }
+
+        public string ReversedString (string givenString, ref int n)
+        {
+            string reversedString = "";
+            string charFromInitialString = "";
+            if (n > 0)
+            {
+                charFromInitialString = ReversedString(givenString,n-1);
+                
+            }
+            return reversedString + charFromInitialString;
+        }
+
+    }
+}
