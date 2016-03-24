f7507d93623364edc3b6d4b52ad6020e161dcc8f
LauFodorean <laurean_fodorean@yahoo.com>
2016-03-24 08:14:35 +0200

    Fibonacci recursiv
    Merge .

---
+++ FibonacciRecursiv/FibonacciRecursiv/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciRecursiv
+{
+    [TestClass]
+    public class UnitTest1
+    {
+         [TestMethod]
+        public void CheckOfFibonacciGeneration()
+        {
+           Assert.AreEqual(5, Fibonacci(5));
+        }
+    
+        public int Fibonacci(int n) 
+        {
+        int previous = 0;
+        return Fibonacci(n, ref previous);
+        }
+
+        public int Fibonacci(int n, ref int previous) 
+        {
+        if (n < 2) return n;
+        int beforePrevious = 0;
+        previous = Fibonacci(n - 1, ref beforePrevious);
+        return previous + beforePrevious;
+        }
+    }
+}
