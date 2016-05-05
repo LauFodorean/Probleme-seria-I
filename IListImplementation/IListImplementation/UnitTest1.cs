909cbb46441c0503f948abdbe25325557b07cdbe
LauFodorean <laurean_fodorean@yahoo.com>
2016-05-05 08:59:35 +0300

    Implementare IList
    De intrebat cum incep. Ar trebui sa fie o functie main ...

---
+++ IListImplementation/IListImplementation/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace IListImplementation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void insertPositionInArrayTestMetod()
+        {
+            object[] givenStringArray = {"one"};
+            Assert.AreEqual("one", Add(givenStringArray[givenStringArray.Length-1]));
+        }
+    }
+
+    public class SimpleList: IList
+    {
+        private object[] _contents = new object[8];
+        private int _count;
+
+        public int SimpleList()
+        {
+           return _count = 0;
+        }
+
+        public int Add (object value)
+        {
+            return 0;
+        }
+    }
+}
