5499b07ecc20261f20394f70bc11667674d0681e
LauFodorean <laurean_fodorean@yahoo.com>
2016-05-10 07:31:57 +0300

    IListImplementation
    Merge metoda ADD, desi acolo trebuie sa refactorizez pentru cazul in
    care ajung la ultima pozitie a arrayului, ca sa imi dubleze. Nu merge
    Clear.

--- IListImplementation/IListImplementation/UnitTest1.cs
+++ IListImplementation/IListImplementation/UnitTest1.cs
@@ -8,26 +8,112 @@ namespace IListImplementation
     public class UnitTest1
     {
         [TestMethod]
-        public void insertPositionInArrayTestMetod()
+        public void AddPosition()
         {
-            object[] givenStringArray = {"one"};
-            Assert.AreEqual("one", Add(givenStringArray[givenStringArray.Length-1]));
+            SimpleListClass list = new SimpleListClass();
+            Assert.AreEqual(list.contents[0] = 1, list.Add(1));
+        }
+
+        [TestMethod]
+        public void ClearPositions()
+        {
+            SimpleListClass list = new SimpleListClass();
+            list.contents[8] = {"1"};
+            list.position = 2;
+            CollectionAssert.AreEqual(new list.contents[8], list.Clear());
+
         }
     }
 
-    public class SimpleList: IList
+    public class SimpleListClass: IList
     {
-        private object[] _contents = new object[8];
-        private int _count;
-
-        public int SimpleList()
+        public object[] contents = new object[8];
+        public int position = 0;
+        
+        public int Add(object value)
         {
-           return _count = 0;
+            position++;
+            int returnValue = (int)(contents[position - 1] = value);
+            return returnValue;
         }
 
-        public int Add (object value)
+        public void Clear()
         {
-            return 0;
+            for (int i = 0; i < contents.Length; i++)
+                contents[i] = null;
+            position = 0;
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFixedSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
