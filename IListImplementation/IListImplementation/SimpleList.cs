7b9f3a066a25405af7f0a37ce71d24ff48c8bd2e
LauFodorean <laurean_fodorean@yahoo.com>
2016-05-26 06:36:08 +0300

    IListImplementation
    Am separat testele de Clasa SilpeList. Am creat in proiect o noua clasa
    SimpleList separat de UnitTest.

---
+++ IListImplementation/IListImplementation/SimpleList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IListImplementation
+{
+    public class SimpleList : IList
+    {
+        private object[] contents = new object[8];
+        private int position;
+
+        public SimpleList(object[] contents, int position)
+        {
+            this.contents = contents;
+            this.position = position;
+        }
+
+        public int Add(object value)
+        {
+            if (position >= contents.Length - 1) Array.Resize(ref contents, contents.Length * 2);
+            if (position < contents.Length)
+            {
+                contents[position] = value;
+                position += 1;
+                return position - 1;
+            }
+
+            return -1;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i <= position; i++)
+                contents[i] = null;
+            position = 0;
+        }
+
+        public bool Contains(object value)
+        {
+            bool result = false;
+            for (int i = 0; i <= position; i++)
+            {
+                if (contents[i].Equals(value)) result = true;
+            }
+            return result;
+        }
+
+        public int IndexOf(object value)
+        {
+            int theIndex = -1;
+            for (int i = 0; i <= position; i++)
+                if (contents[i].Equals(value))
+                {
+                    return theIndex = i;
+                }
+
+            return theIndex;
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFixedSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Remove(object value)
+        {
+            RemoveAt(IndexOf(value));
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < position))
+            {
+                for (int j = index; j < position - 1; j++)
+                { contents[j] = contents[j + 1]; }
+                position--;
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { return position; }
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < position; i++)
+                yield return contents[i];
+        }
+    }
+}
---
+++ IListImplementation/IListImplementation/UnitTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace IListImplementation
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void AddPosition()
+        {
+            SimpleList list = new SimpleList(new object[8], 1);
+            Assert.AreEqual(1, list.Add(1));
+        }
+
+        [TestMethod]
+        public void AddPositionWhenArrayLengthDoubles()
+        {
+            SimpleList list = new SimpleList(new object[8] { 1, 2, 3, 4, 5, 6, 7, 8 }, 8);
+            Assert.AreEqual(8, list.Add(9));
+        }
+
+        [TestMethod]
+        public void listContainsASpecificValue()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 7, 8 }, 2);
+            Assert.AreEqual(true, list.Contains(7));
+        }
+
+        [TestMethod]
+        public void ClearList()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 2, 3 }, 2);
+            SimpleList expectedResult = new SimpleList(new object[] { }, 0);
+            list.Clear();
+            CollectionAssert.AreEqual(expectedResult,list);
+
+        }
+
+        [TestMethod]
+        public void RemoveObjectFromList()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 7);
+            SimpleList newList = new SimpleList(new object[] { 1, 2, 4, 5, 6, 7, 8 }, 6);
+            list.Remove(3);
+            CollectionAssert.AreEqual(newList, list);
+
+        }
+
+        [TestMethod]
+        public void RemoveObjectFromSpecifiedPosition()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 7);
+            SimpleList newList = new SimpleList(new object[] { 1, 2, 4, 5, 6, 7, 8 }, 6);
+            list.RemoveAt(2);
+            CollectionAssert.AreEqual(newList, list);
+
+        }
+
+        [TestMethod]
+        public void ObtainTheIndex()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 7);
+            Assert.AreEqual(3, list.IndexOf(4));
+        }
+
+        [TestMethod]
+        public void CountElementsInArray()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 7);
+            int numberOfElements = list.Count;
+            Assert.AreEqual(7, numberOfElements);
+        }
+
+        [TestMethod]
+        public void CopyElementsToAnArrayStartingFromAPosition()
+        {
+            SimpleList list = new SimpleList(new object[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 7);
+        }
+
+    }
+      
+}
