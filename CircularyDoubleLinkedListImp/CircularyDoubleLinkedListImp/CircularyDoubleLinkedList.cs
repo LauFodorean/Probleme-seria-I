b0d5ffe03038107991bb039bdb0aac255643f093
LauFodorean <laurean_fodorean@yahoo.com>
2016-06-09 06:40:18 +0300

    CicularyDoubleLinkedList
    Metoda addNode.

---
+++ CircularyDoubleLinkedListImp/CircularyDoubleLinkedListImp/CircularyDoubleLinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularyDoubleLinkedListImp
+{
+    public class CircularyDoubleLinkedList
+    {
+        private Node head;
+        private Node dlList;
+        private int count;
+
+        public CircularyDoubleLinkedList()
+        {
+            count = 0;
+        }
+        
+        public class Node
+        {
+            public Node next;
+            public Node previous;
+            public int value;
+            
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        public void addNode(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (count ==0)
+            {
+                dlList = newNode;
+                dlList.next = dlList;
+                dlList.previous = dlList;
+                head = dlList;
+            }
+            else
+            {
+                newNode.previous = dlList.previous;
+                newNode.next = dlList;
+                dlList.previous.next = newNode;
+                dlList.previous = newNode;
+                head = dlList;
+            }
+            count++;
+        }
+    }
+}
---
+++ CircularyDoubleLinkedListImp/CircularyDoubleLinkedListImp/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircularyDoubleLinkedListImp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddElementToEmptyList()
+        {
+            CircularyDoubleLinkedList list = new CircularyDoubleLinkedList();
+            list.addNode(1);
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
